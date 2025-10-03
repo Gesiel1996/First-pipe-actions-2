b88c69e2306b0c48cc4de7e2ee3cbd067e876acc
gesiel.pereira <gesiel.pereira@globalhitss.com.br>
2025-10-03 15:22:16 -0300

    subindo novo codigo6

--- tf-infra/backend.tf
+++ tf-infra/backend.tf
@@ -1,10 +1,9 @@
 terraform {
  backend "s3" {
-
-  bucket         = "my-terraform-state-bucket"
+  bucket         = "gesiel1996-hithub-actions"
     key            = "path/to/my/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "my-terraform-lock-table" # Optional, for state locking
+    dynamodb_table = "gesiel1996-sa-east-1-terraform-lock" # Optional, for state locking
     encrypt        = true
 
  }
