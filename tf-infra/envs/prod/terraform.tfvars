b2d295140ff0b873a05eb80230cad3f0645def4d
gesiel.pereira <gesiel.pereira@globalhitss.com.br>
2025-10-03 16:10:51 -0300

    crinado novo ambiente

---
+++ tf-infra/envs/prod/terraform.tfvars
@@ -0,0 +1,1 @@
+bucket_name="prod-github-actions-1241564546514"
--- tf-infra/main.tf
+++ tf-infra/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "github-actions-1241564546514" {
+resource "aws_s3_bucket" "-prod-github-actions-1241564546514" {
   bucket = var.bucket_name
 }
 
