04306a2032b1568f738f5aa244cd955e399f9abf
gesiel.pereira <gesiel.pereira@globalhitss.com.br>
2025-10-03 15:57:17 -0300

    subindo novo codigo

--- tf-infra/main.tf
+++ tf-infra/main.tf
@@ -1,9 +1,4 @@
 resource "aws_s3_bucket" "github-actions-1241564546514" {
   bucket = var.bucket_name
-
-  tags = {
-    Name        = "My example bucket for GitHub Actions"
-    Environment = "main"
-  }
 }
 
