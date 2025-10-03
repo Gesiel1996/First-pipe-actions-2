7d32dfcc19c472e10d3a6806d9e9201def4436b4
gesiel.pereira <gesiel.pereira@globalhitss.com.br>
2025-10-03 15:05:26 -0300

    subindo novo codigo3

--- tf-infra/provider.tf
+++ tf-infra/provider.tf
@@ -3,11 +3,9 @@ terraform {
 required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
       region = "us-east-1"
 
     }
   }
-  required_version = ">= 1.0.0"
   
 }
